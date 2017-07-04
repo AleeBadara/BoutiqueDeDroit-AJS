1edf99051228450956d0edb7411182572bb4004d
Alioune Badara Mbaye <ambaye@csc.com>
2017-07-04 17:16:40 +0200

    organisation des champs de filtres

--- app/components/SuiviCas.jsx
+++ app/components/SuiviCas.jsx
@@ -14,28 +14,28 @@ class SuiviCas extends React.Component {
                                 <div className="small-3 columns">
                                     <TextField required id="nom" label="Nom" type="text" className="txtField" marginForm />
                                 </div>
-                                <div className="small-3 columns">
+                                <div className="small-3 columns small-offset-2">
                                     <TextField required id="nom" label="Prénom" type="text" className="txtField" marginForm />
                                 </div>
-                                <div className="small-6 columns"></div>
+                                <div className="small-4 columns"></div>
                             </div>
                             <div className="row">
                                 <div className="small-3 columns">
                                     <TextField id="dateCreationDebut" label="Date création entre" type="date" defaultValue="2017-05-24" className="txtField" marginForm />
                                 </div>
-                                <div className="small-3 columns">
+                                <div className="small-3 columns small-offset-2">
                                     <TextField id="dateCreationFin" label="et le" type="date" defaultValue="2017-05-24" className="txtField" marginForm />
                                 </div>
-                                <div className="small-6 columns"></div>
+                                <div className="small-4 columns"></div>
                             </div>
                             <div className="row">
                                 <div className="small-3 columns">
                                     <TextField id="dateModificationDebut" label="Date modification entre" type="date" defaultValue="2017-05-24" className="txtField" marginForm />
                                 </div>
-                                <div className="small-3 columns">
+                                <div className="small-3 columns small-offset-2">
                                     <TextField id="dateModificationFin" label="et le" type="date" defaultValue="2017-05-24" className="txtField" marginForm />
                                 </div>
-                                <div className="small-6 columns"></div>
+                                <div className="small-4 columns"></div>
                             </div>
                         </fieldset>
                     </div>
