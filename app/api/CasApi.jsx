57ceb609992c29ca13de8f89941d4c0437d68ee4
Alioune Badara Mbaye <ambaye@csc.com>
2017-07-13 17:41:19 +0200

    API addNewCas

--- app/api/CasApi.jsx
+++ app/api/CasApi.jsx
@@ -1,14 +1,23 @@
+let seedData = [
+    { id: 1, nom: 'Kane', prenom: 'Fatou', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 2, nom: 'Niang', prenom: 'Astou', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 3, nom: 'Diallo', prenom: 'Aminata', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 4, nom: 'Ndiaye', prenom: 'Kiné', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' }
+];
+
 var getAllCas = () => {
-    let results = [
-        { id: 1, nom: 'Kane', prenom: 'Fatou', type: 'Divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-        { id: 2, nom: 'Niang', prenom: 'Astou', type: 'Divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-        { id: 3, nom: 'Diallo', prenom: 'Aminata', type: 'Divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-        { id: 4, nom: 'Ndiaye', prenom: 'Kiné', type: 'Divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' }
-    ];
-    return results;
+    return seedData;
 
 };
 
+var addNewCas = (cas) => {
+    let newCas = { ...cas }
+    console.log('API');
+    console.log(newCas);
+    seedData.push(newCas);
+};
+
 module.exports = {
-    getAllCas
+    getAllCas,
+    addNewCas
 };
