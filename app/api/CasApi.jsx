ef6b5bc49c26ff08721eb964087e82af0813e708
Alioune Badara Mbaye <ambaye@csc.com>
2017-07-20 11:54:33 +0200

    suppression des console.log

--- app/api/CasApi.jsx
+++ app/api/CasApi.jsx
@@ -1,19 +1,16 @@
 let seedData = [
-    { id: 1, nom: 'Kane', prenom: 'Fatou', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-    { id: 2, nom: 'Niang', prenom: 'Astou', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-    { id: 3, nom: 'Diallo', prenom: 'Aminata', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
-    { id: 4, nom: 'Ndiaye', prenom: 'Kiné', type: 'Divorce', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' }
+    { id: 1, nom: 'Kane', prenom: 'Fatou', type: 'Divorce', sexe: 'F', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 2, nom: 'Niang', prenom: 'Astou', type: 'Divorce', sexe: 'F', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 3, nom: 'Diallo', prenom: 'Aminata', type: 'Divorce', sexe: 'F', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' },
+    { id: 4, nom: 'Ndiaye', prenom: 'Kiné', type: 'Divorce', sexe: 'F', cas: 'Procédure de divorce', description: 'Mme est en instance de divorce et demande des informations sur le processus de divorce', createdBy: { nom: 'Dieng', prenom: 'Anta' }, etat: 'En cours' }
 ];
 
 var getAllCas = () => {
     return seedData;
-
 };
 
 var addNewCas = (cas) => {
     let newCas = { ...cas }
-    console.log('API');
-    console.log(newCas);
     seedData.push(newCas);
 };
 
